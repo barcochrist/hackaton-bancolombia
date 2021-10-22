bbff1c5d9903c679e6f080f8534c8ebe05717cbe
Christian Barco <barcochrist@gmail.com>
2021-10-22 18:06:19 -0500

    (init): create basic project structure

---
+++ src/main/java/co/programacionmaster/cenaclientes/CenaClientesApplication.java
@@ -0,0 +1,13 @@
+package co.programacionmaster.cenaclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenaClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenaClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/programacionmaster/cenaclientes/CenaClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.programacionmaster.cenaclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenaClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e084c5a7fd54321f22c6cee169a7fe4b1898355d
Christian Barco <barcochrist@gmail.com>
2021-10-23 08:21:19 -0500

    (dependency): add new dependencies and mysql configurations

--- src/main/java/co/programacionmaster/cenaclientes/CenaClientesApplication.java
+++ src/main/java/co/programacionmaster/cenaclientes/CenaClientesApplication.java
@@ -1,13 +1,29 @@
 package co.programacionmaster.cenaclientes;
 
+import co.programacionmaster.cenaclientes.controller.ClientRestController;
+import co.programacionmaster.cenaclientes.entity.ClientJpa;
+import co.programacionmaster.cenaclientes.repository.ClientRepository;
+import co.programacionmaster.cenaclientes.service.impl.ClientServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = {
+    ClientServiceImpl.class,
+    ClientRestController.class
+})
+@EntityScan(basePackageClasses = {
+    ClientJpa.class
+})
+@EnableJpaRepositories(basePackageClasses = {
+    ClientRepository.class
+})
 public class CenaClientesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CenaClientesApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(CenaClientesApplication.class, args);
+  }
 }

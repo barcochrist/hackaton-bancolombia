72b13d9e44d075917bfa44d70cabfa42b4a6715d
Christian Barco <barcochrist@gmail.com>
2021-10-23 16:01:50 -0500

    (entity): delete unnecessary entities and repositories

--- src/main/java/co/programacionmaster/cenaclientes/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package co.programacionmaster.cenaclientes.repository;
-
-import co.programacionmaster.cenaclientes.entity.AccountJpa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<AccountJpa, Integer> {
-
-}
